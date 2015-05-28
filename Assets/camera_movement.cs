454a8b7d6f729bcf1e52cd139ace42d220e71499
yacinebenz <yacinebenz3@gmail.com>
2015-05-27 17:52:52 -0700

    changed the dot movement

---
+++ Assets/camera_movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_movement : MonoBehaviour {
+	public GameObject targetl;
+	public float xOffset = 0;
+	public float yOffset = 0;
+	public float zOffset = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		this.transform.position = new Vector3(targetl.transform.position.x + xOffset,
+		                                      targetl.transform.position.y + yOffset,
+		                                      targetl.transform.position.z + zOffset);
+	}
+}
